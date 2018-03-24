56a30a4817bd37cb118afa4f0e10db748ec82a4b
Artik <artik.alpha@gmail.com>
2018-03-25 00:24:59 +0300

    UPD: camera preview (bugs)

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,54 @@
+var elements = {};
+
+var app = {
+  // https://github.com/cordova-plugin-camera-preview/cordova-plugin-camera-preview
+  startCamera: function () {
+    CameraPreview.startCamera({
+      width: window.screen.width,
+      height: window.screen.height / 2,
+      camera: "front",
+      tapPhoto: false,
+      tapFocus: true,
+      previewDrag: false,
+      toBack: true
+    });
+  },
+  takePicture: function () {
+    CameraPreview.takePicture(function (imgData) {
+      elements.picture.src = 'data:image/jpeg;base64,' + imgData;
+      elements.form.toggleClass('hidden');
+    });
+  },
+  switchCamera: function () {
+    CameraPreview.switchCamera();
+  },
+  savePicture: function () {
+
+  },
+  init: function () {
+    // CameraPreview.setPreviewSize({
+    //   width: window.screen.width,
+    //   height: window.screen.height / 2
+    // })
+
+    elements.takePicture.addEventListener('click', this.takePicture, false);
+    elements.switchCamera.addEventListener('click', this.switchCamera, false);
+    this.startCamera();
+  }
+
+};
+
+
+
 document.addEventListener('deviceready', function () {
+  elements = {
+    takePicture: document.getElementById('takePicture'),
+    switchCamera: document.getElementById('switchCamera'),
+    picture: document.getElementById('originalPicture'),
+    form: document.getElementById('form')
+  };
+
+  app.init();
+
 
 }, false);
