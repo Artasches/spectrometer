c9d6b7fb88758378b313e857c092b64224f2e376
Artik <artik.alpha@gmail.com>
2018-04-20 22:42:55 +0300

    --

--- api/redirect.heroku.php
+++ api/redirect.heroku.php
@@ -7,20 +7,20 @@ require_once '../vendor/autoload.php';
 
 use HerokuClient\Client as HerokuClient;
 
-// $heroku = new HerokuClient([
-//     'apiKey' => 'be4d186c-d7f5-4bf0-b71b-9ff84b450783',
-//     'curlOptions' => [
-//         CURLOPT_URL => 'http://artik.me/spectrometer/api/',
-//         CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13',
-//         CURLOPT_RETURNTRANSFER => true,
-//         CURLOPT_AUTOREFERER => true,
-//         CURLOPT_FOLLOWLOCATION => true,
-//         CURLOPT_MAXREDIRS => 20,
-//         CURLOPT_POST => true,
-//         CURLOPT_POSTFIELDS => $_POST,
-//     ],
-// ]);
+$heroku = new HerokuClient([
+    'apiKey' => 'be4d186c-d7f5-4bf0-b71b-9ff84b450783',
+    'curlOptions' => [
+        CURLOPT_URL => 'http://artik.me/spectrometer/api/',
+        CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_AUTOREFERER => true,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_MAXREDIRS => 20,
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => $_POST,
+    ],
+]);
 
-// var_dump($heroku->post('apps/artik-spectrometer/formation/web', $_POST));
+var_dump($heroku->post('apps/artik-spectrometer/formation/web', $_POST));
 
 echo "heroku";
