616774e523ed0497f3c303038598105cae94ad1e
Artik <artik.alpha@gmail.com>
2018-04-20 22:29:35 +0300

    --

--- api/redirect.heroku.php
+++ api/redirect.heroku.php
@@ -3,7 +3,7 @@ ini_set('max_input_vars', 1000000);
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-require_once __DIR__ . '../vendor/autoload.php';
+require_once '../vendor/autoload.php';
 
 // use HerokuClient\Client as HerokuClient;
 
