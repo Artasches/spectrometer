becc9e2e7eb7d82d3370cb7e153062c721d1f851
Artik <artik.alpha@gmail.com>
2018-04-20 23:29:17 +0300

    UPD: heroku

--- api/redirect.heroku.php
+++ api/redirect.heroku.php
@@ -5,24 +5,6 @@ header('Content-Type: application/json');
 
 require_once '../vendor/autoload.php';
 
-// $heroku = new HerokuClient\Client();
-// $heroku = new HerokuClient([
-//     'apiKey' => 'be4d186c-d7f5-4bf0-b71b-9ff84b450783',
-//     'curlOptions' => [
-//         CURLOPT_URL => 'http://artik.me/spectrometer/api/',
-//         CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13',
-//         CURLOPT_RETURNTRANSFER => true,
-//         CURLOPT_AUTOREFERER => true,
-//         CURLOPT_FOLLOWLOCATION => true,
-//         CURLOPT_MAXREDIRS => 20,
-//         CURLOPT_POST => true,
-//         CURLOPT_POSTFIELDS => $_POST,
-//     ],
-// ]);
-
-// var_dump($heroku->post('apps/artik-spectrometer/formation/web', $_POST));
-// var_dump($heroku);
-
 function poster($url, $fields_string)
 {
     $ua = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13';
@@ -43,4 +25,3 @@ function poster($url, $fields_string)
     return $result;
 }
 echo poster('http://artik.me/spectrometer/api/', $_POST);
-echo "heroku";
